e33bfb7c227dcd6446d9c171e6aa2505bdce95d0
judgejab <mattcleary@gmail.com>
2017-08-02 18:30:25 +0100

    finished pluralsight course

---
+++ src/api/baseUrl.js
@@ -0,0 +1,13 @@
+export default function baseUrl() {
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001' : 'https://calm-fortress-91172.herokuapp.com/';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
